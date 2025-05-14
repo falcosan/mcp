37f3782e8c1488daeab6a46e91415790f135cbf7
Daniele Falchetti <danielefalche@gmail.com>
2025-05-14 20:47:38 +0200

    refactor: update environment variable references for Meilisearch host and API key

--- src/stdio.ts
+++ src/stdio.ts
@@ -2,6 +2,6 @@ import { mcpMeilisearchServer } from "./index.js";
 
 mcpMeilisearchServer({
   transport: "stdio",
-  meilisearchHost: process.env.VITE_MEILISEARCH_HOST!,
-  meilisearchApiKey: process.env.VITE_MEILISEARCH_API_KEY!,
+  meilisearchHost: process.env.MEILISEARCH_HOST!,
+  meilisearchApiKey: process.env.MEILISEARCH_API_KEY!,
 });
