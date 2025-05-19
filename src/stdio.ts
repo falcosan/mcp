3146252ac93442f1369068934d46ee30a7060633
Daniele Falchetti <danielefalche@gmail.com>
2025-05-19 13:32:10 +0200

    refactor: Simplify mcpMeilisearchServer invocation by removing unnecessary options

--- src/stdio.ts
+++ src/stdio.ts
@@ -1,6 +1,3 @@
 import { mcpMeilisearchServer } from "./index.js";
 
-mcpMeilisearchServer({
-  transport: "stdio",
-  meilisearchHost: process.env.MEILISEARCH_HOST,
-});
+await mcpMeilisearchServer({ transport: "stdio" });
