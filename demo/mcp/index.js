70126a9e6e4b66dc844df57657eebf14a2aa571c
Daniele Falchetti <danielefalche@gmail.com>
2025-05-21 12:48:54 +0200

    fix: Update AI provider and model in mcpMeilisearchServer configuration

--- demo/mcp/index.js
+++ demo/mcp/index.js
@@ -1,8 +1,8 @@
 import { mcpMeilisearchServer } from "../../dist/index.js";
 
 await mcpMeilisearchServer({
-  aiProviderName: "ollama",
-  llmModel: "qwen3:14b",
+  aiProviderName: "openrouter",
+  llmModel: "meta-llama/llama-4-maverick:free",
   meilisearchHost: process.env.VITE_MEILISEARCH_HOST,
   aiProviderApiKey: process.env.VITE_AI_PROVIDER_API_KEY,
   meilisearchApiKey: process.env.VITE_MEILISEARCH_API_KEY,
